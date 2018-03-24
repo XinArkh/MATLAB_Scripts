354e2828116cd6c8fdb0a1d0756f90359dc1374a
DELL- <wuxin.zju@qq.com>
2018-03-24 16:22:05 +0800

    Fix Fatal Errors(damnit!)

--- Prof.Klaus/project1/hand_in_codes/sysmodel.m
+++ Prof.Klaus/project1/hand_in_codes/sysmodel.m
@@ -9,9 +9,7 @@ function x_new = sysmodel ( r, h, x_k, flag )
         f = 1 + m + 1 / 2 * m^2 + 1 / 6 * m^3;
     elseif flag == 2
         % EULER
-        sprintf('here')
-        m
-        f = 1 + m
+        f = 1 + m;
     elseif flag == 3
         % TRAPEZOID
         f = (1 + 0.5 * m) / (1 - 0.5 * m);
